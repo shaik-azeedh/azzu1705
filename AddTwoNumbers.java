732bfc2c08edd85dcb436ed34e166f518e61816e
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-05-25 22:31:55 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
+         System.out.println("Welcome to java World");
+       
         int a=3000;
         int b = 5000;
         int c = a+b;
         System.out.println("*************************************************");
         System.out.println("The Addition of a and b is :" +c);
-        System.out.println("**************************************************")
-     System.out.println("Welcome to java World");
+        System.out.println("**************************************************");
+    
     }
 }
