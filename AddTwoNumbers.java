9607b31d1305e2ac656e2eb91255114daaad62bf
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-05-25 22:30:46 +0530

    Modify AddTwoNumbers to change input values and format
    Updated the values of a and b, added decorative lines.

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int a=30;
-        int b = 50;
+        int a=3000;
+        int b = 5000;
         int c = a+b;
+        System.out.println("*************************************************");
         System.out.println("The Addition of a and b is :" +c);
+        System.out.println("**************************************************")
      System.out.println("Welcome to java World");
     }
 }
