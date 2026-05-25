13a3595a0ee9e5d2d41a3e78ffc29ec46b2b31d2
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-05-25 22:27:17 +0530

    Implement addition of two numbers in AddTwoNumbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
+        int a=30;
+        int b = 50;
+        int c = a+b;
+        System.out.println("The Addition of a and b is :" +c);
      System.out.println("Welcome to java World");
     }
 }
